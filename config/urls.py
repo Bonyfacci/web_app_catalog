9a51175d73efbceca2a52241676c7bbefd0bd0bb
Сергей Платонов <platonovpochta@gmail.com>
2023-08-15 04:18:25 +0200

    Homework 3

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('my_project/', include('my_project.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
