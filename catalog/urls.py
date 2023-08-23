db19d61ec45ad4754c182df3f6a01dad49550530
Сергей Платонов <platonovpochta@gmail.com>
2023-08-23 19:23:10 +0200

    Homework-5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products, article
+from catalog.views import HomeListView, contacts, ProductListView, ProductCreateView, ProductUpdateView, \
+    ArticleDetailView, ProductDeleteView, NewsListView, NewsCreateView, NewsUpdateView, NewsDeleteView, \
+    NewsArticleDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('<int:pk>/products/', products, name='products'),
-    path('<int:pk>/article/', article, name='article'),
+    path('products/<int:pk>/', ProductListView.as_view(), name='products'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/edit/<int:pk>/', ProductUpdateView.as_view(), name='product_edit'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article'),
+    path('news/', NewsListView.as_view(), name='news'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
+    path('news/edit/<int:pk>/', NewsUpdateView.as_view(), name='news_edit'),
+    path('news/delete/<int:pk>/', NewsDeleteView.as_view(), name='news_delete'),
+    path('news/article/<int:pk>/', NewsArticleDetailView.as_view(), name='news_article'),
 ]
