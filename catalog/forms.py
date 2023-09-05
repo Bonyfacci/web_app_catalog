97a597fa6d237d8fa46cc590aeafb16c1626d8a7
Сергей Платонов <platonovpochta@gmail.com>
2023-09-06 01:53:25 +0200

    Homework-6

---
+++ catalog/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+
+from catalog.models import Product, News, Version
+
+
+class StyleFormMixin:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def clean_name(self):
+        word_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data['name']
+        for i in word_list:
+            if i in cleaned_data:
+                raise forms.ValidationError(f"Нельзя использовать в названии и описании такие слова, как: "
+                                            f"{' '.join(word_list)}")
+
+        return cleaned_data
+
+    def clean_description(self):
+        word_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data['description']
+        for i in word_list:
+            if i in cleaned_data:
+                raise forms.ValidationError(f"Нельзя использовать в названии и описании такие слова, как: "
+                                            f"{' '.join(word_list)}")
+
+        return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['current_version'].widget.attrs.update({'class': 'form-check-input'})
+
+    class Meta:
+        model = Version
+        fields = '__all__'
+
+
+class NewsForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = News
+        fields = '__all__'
